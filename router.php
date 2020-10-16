80525eb2e3f264c8517696a7422cdd3a349248d5
Agustina Notti <agustinantt@gmail.com>
2020-10-16 12:33:49 -0300

    agrego rutas de login

--- router.php
+++ router.php
@@ -1,9 +1,12 @@
 <?php
     require_once 'Controller/TasksController.php';
+    require_once 'Controller/UserController.php';
     require_once 'RouterClass.php';
     
     // CONSTANTES PARA RUTEO
     define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/');
+    define("LOGIN", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/login');
+   
 
     $r = new Router();
 
@@ -16,8 +19,13 @@
     $r->addRoute("delete/:ID", "GET", "TasksController", "DeleteTask");
     $r->addRoute("completar/:ID", "GET", "TasksController", "MarkAsCompletedTask");
 
+
+    //### LOGIN
+    $r->addRoute("login", "GET", "UserController", "Login");
+    $r->addRoute("verifyUser", "POST", "UserController", "VerifyUser");
+
     //Ruta por defecto.
-    $r->setDefaultRoute("TasksController", "Home");
+    $r->setDefaultRoute("UserController", "Login");
 
     //Advance
     $r->addRoute("autocompletar", "GET", "TasksController", "AutoCompletar");
